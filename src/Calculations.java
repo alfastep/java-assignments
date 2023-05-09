8e53a1c11f468fe50e36a19c410c7880a0190535
Stephen Alfa <stojalfa@gmail.com>
2023-05-09 11:51:12 -0400

    Calculations assignment

---
+++ src/Calculations.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter another number: ");
+
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + ((float)num1 / (float)num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+    }
+}
