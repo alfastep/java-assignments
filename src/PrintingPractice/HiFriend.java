3dc90e42e0e3098902c30be01d282d30aa24e7c2
Stephen Alfa <stojalfa@gmail.com>
2023-05-09 14:13:54 -0400

    Moved printing practice assignment to this repo

---
+++ src/PrintingPractice/HiFriend.java
@@ -0,0 +1,8 @@
+package PrintingPractice;
+
+public class HiFriend {
+    public static void main(String[] args) {
+        String message = "Hi Larry Sprinkle!";
+        System.out.println(message);
+    }
+}
